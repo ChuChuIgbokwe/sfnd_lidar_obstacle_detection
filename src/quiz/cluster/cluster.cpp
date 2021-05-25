1b2809ad2c850ddd8994b829881ae0e6fa858245
Chu-Chu Igbokwe <raider64x@gmail.com>
2021-05-25 12:43:36 -0700

    Implemented 2d euclidean clustering

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -73,13 +73,43 @@ render2DTree(Node *node, pcl::visualization::PCLVisualizer::Ptr &viewer, Box win
 
 }
 
+void
+clusterHelper(int index, const std::vector<std::vector<float>> points , std::vector<int>& cluster,
+              std::vector<bool>& processed, KdTree *tree, float distanceTol)
+{
+    processed[index] = true;
+    cluster.push_back(index);
+
+    std::vector<int> nearestPointId = tree->search(points[index], distanceTol);
+
+    for( auto id: nearestPointId)
+    {
+        if (!processed[id]){
+            clusterHelper(id, points, cluster, processed, tree, distanceTol);
+        }
+    }
+}
+
 std::vector<std::vector<int>>
 euclideanCluster(const std::vector<std::vector<float>> &points, KdTree *tree, float distanceTol) {
 
     // TODO: Fill out this function to return list of indices for each cluster
-
     std::vector<std::vector<int>> clusters;
-
+    std::vector<bool> processed(points.size(), false);
+    int i = 0;
+    while(i < points.size())
+    {
+        if (processed[i])
+        {
+            i++;
+            continue;
+        }
+        // If point has not been processed Create cluster
+        std::vector<int> cluster;
+        clusterHelper(i, points, cluster, processed, tree, distanceTol);
+        clusters.push_back(cluster);
+        i++;
+    }
     return clusters;
 
 }
